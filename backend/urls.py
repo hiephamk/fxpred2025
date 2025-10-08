07c281aa54ad6124e4c8a465ed6472c79779fce3
hiephamk <hiep23000@student.hamk.fi>
2025-10-08 23:21:21 +0300

    fix urls frontend

--- backend/urls.py
+++ backend/urls.py
@@ -1,12 +1,12 @@
-
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Prefer myfx endpoints when present
     path('api/', include('myfx.urls')),
-    path('api/', include('myfx.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
-]
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('home/forex', TemplateView.as_view(template_name='index.html'), name='forex'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- myfx/urls.py
+++ myfx/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-# from .views import train_nn_view, forecast_nn
 from . import views
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from django.conf import settings
 
-# urlpatterns = [
-#     path("forex/train_nn/", train_nn_view),
-#     path("forex/forecast/", forecast_nn)
-# ]
 
 urlpatterns = [
     path('forex/train/', views.train_nn_view, name='train_nn'),
@@ -16,4 +14,5 @@ urlpatterns = [
     path('forex/real-data/latest-data/', views.fetch_latest_data, name='latest_real_data'),
     path('forex/predictions/history/', views.PredictedForexDataView.as_view(), name='predictions_history'),
     path('forex/real_data/', views.ForexDataView.as_view(), name='forex_real_data'),
-]
+    path('', TemplateView.as_view(template_name='index.html')),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
